380c5f866dabccefc3b22b999b4f58e1b5025509
l3akage <info@martinpoppen.de>
2015-12-05 22:12:03 +0100

    initial

---
+++ Readme.md
@@ -0,0 +1,21 @@
+# OTRS2Slack
+
+Post OTRS tickets to [Slack](https://slack.com/)
+
+# Setup
+
+set necessary variables
+``` ruby
+WEBHOOK_URL => Slack [webook url](https://my.slack.com/services/new/incoming-webhook
+)
+OTRS_HOST => your OTRS url
+HOST, USER, PASSWORD and DATABSE of the database used by OTRS
+```
+
+# Integrate
+
+Login into OTRS and go to Admin => GenericAgent and create a new one.
+Set up the events that should trigger the script and add this script to CMD.
+``` ruby
+/opt/otrs2slack.rb
+```
---
+++ otrs2slack.rb
@@ -0,0 +1,26 @@
+#! /usr/bin/env ruby
+
+require 'slack-notifier'
+require 'mysql2'
+
+WEBHOOK_URL = 'WEBHOOK URL'
+OTRS_HOST   = 'http://example.com'
+
+exit if ARGV.size != 2
+
+client = Mysql2::Client.new(host: 'SQL HOST',
+                            username: 'SQL USER',
+                            password: 'SQL PASSWORD',
+                            database: 'SQL DATABASE')
+
+row = client.query("SELECT id, tn, title, customer_id FROM ticket WHERE tn = '#{ARGV[0]}'").first
+
+notifier = Slack::Notifier.new(WEBHOOK_URL)
+notifier.channel  = '#otrs'
+notifier.username = 'otrs'
+
+msg = "<#{OTRS_HOST}/index.pl?Action=AgentTicketZoom;TicketID=#{row['id']}|#{row['tn']}>"
+msg = "#{msg} New Ticket from #{row['customer_id']}: #{row['title']}"
+
+notifier.ping(msg, icon_emoji: ':email:')
+client.close
